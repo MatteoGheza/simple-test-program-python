7ebce32779f0c1163a226a6c7cf34df9219ccce0
Matteo Gheza <matteo.gheza07@gmail.com>
2020-08-17 21:27:13 +0200

    fix(release): Fix version.py updater

--- simpletestprogram/version.py
+++ simpletestprogram/version.py
@@ -1,1 +1,3 @@
-__version__ = '0.0.0'
+# DON'T update with manually, always use release trigger from Github Actions. See RELEASES.md
+# NON modificare questo file manualmente, use sempre il "release trigger" da Github Actions. Guarda RELEASES.md
+__version__ = 0.0.0
