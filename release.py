19d49663e1b5ab40036d60e86dce18a79d0248ab
Matteo Gheza <matteo.gheza07@gmail.com>
2020-08-20 15:42:54 +0200

    fix(release): release.py [skip-ci]

--- release.py
+++ release.py
@@ -1,8 +1,9 @@
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="release utils")
 parser.add_argument('--write',  help='Write new version to version.py', action="store_true")
 parser.add_argument('--version',  help='New version')
+parser.add_argument('--artifacts',  help='ZIP dir artifacts', action="store_true")
 
 args = parser.parse_args()
 
@@ -12,3 +13,10 @@ if args.write:
     f.write("# NON modificare questo file manualmente, use sempre il 'release trigger' da Github Actions. Guarda RELEASES.md\n")
     f.write("__version__ = \'"+args.version+"'\n")
     f.close()
+elif args.artifacts:
+    import shutil
+    shutil.make_archive("build_artifacts/windows-latest", 'zip', "build_artifacts/windows-latest")
+    shutil.make_archive("build_artifacts/ubuntu-latest", 'zip', "build_artifacts/ubuntu-latest")
+    shutil.make_archive("build_artifacts/macos-latest", 'zip', "build_artifacts/macos-latest")
+else:
+    print("To release the program, run semantic-release.")
